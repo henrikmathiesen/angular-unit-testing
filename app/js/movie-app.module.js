c7f21077fe8e07a31575c5a7e8f2aa9561dc7981
Henrik <henrik_mathiesen@hotmail.com>
2016-10-02 12:11:29 +0200

    -route

--- app/js/movie-app.module.js
+++ app/js/movie-app.module.js
@@ -1,3 +1,12 @@
 /// <reference path="../../typings/index.d.ts" />
 
-angular.module('movie-app', ['ngRoute', 'ngResource', 'ui.bootstrap']);
+angular.module('movie-app', ['ngRoute', 'ngResource', 'ui.bootstrap'])
+    .config(function ($routeProvider) {
+        
+        $routeProvider
+            .when('/results', {
+                templateUrl: '',
+                controller: 'resultsController as resultsCtrl'
+            })
+            .otherwise({ redirectTo: '/' });
+    });
