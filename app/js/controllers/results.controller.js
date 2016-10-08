5a4cd1b02fe944cdca6d0eba73c6946dec58504a
Henrik <henrik_mathiesen@hotmail.com>
2016-10-08 23:43:37 +0200

    -using directive for search results

--- app/js/controllers/results.controller.js
+++ app/js/controllers/results.controller.js
@@ -8,17 +8,29 @@ angular
 
         var query = $location.search().q;
 
+        var errorHandler = function () {
+            resultsCtrl.errorMessage = "ERROR: something went wrong";
+        };
+
         omdbApi.search(query)
             .then(function (response) {
                 if (!response.data.Error) {
                     resultsCtrl.results = response.data.Search;
                 }
                 else {
-                    resultsCtrl.results = [ { noHits: "No matches" } ];
+                    resultsCtrl.results = [{ noHits: "No matches" }];
                 }
             })
-            .catch(function () {
-                resultsCtrl.errorMessage = "ERROR: something went wrong";
-            });
+            .catch(errorHandler);
+
+        resultsCtrl.toggleAccordian = function (isOpen, id, index) {
+            if (isOpen) {
+                omdbApi.find(id)
+                    .then(function (response) {
+                        resultsCtrl.results[index].movieDetails = response.data;
+                    })
+                    .catch(errorHandler);
+            }
+        };
 
     });
