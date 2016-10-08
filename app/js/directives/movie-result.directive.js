f28e145df1bdbc4f22b22cba7676ee3e2ac4a5d3
Henrik <henrik_mathiesen@hotmail.com>
2016-10-08 20:55:01 +0200

    -unit test directive

--- app/js/directives/movie-result.directive.js
+++ app/js/directives/movie-result.directive.js
@@ -1,1 +1,9 @@
 /// <reference path="../../../typings/index.d.ts" />
+
+angular
+    .module('movie-app')
+    .directive('movieResult', function(){
+        return {
+            template: '<div>Star Wars: Episode IV - A New Hope</div>'
+        }
+    });
--- tst/directives/movie-result.directive.test.js
+++ tst/directives/movie-result.directive.test.js
@@ -3,16 +3,28 @@
 describe("movie-result directive test", function () {
 
     var result = {
-        Poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BOTIyMDY2NGQtOGJjNi00OTk4LWFhMDgtYmE3M2NiYzM0YTVmXkEyXkFqcGdeQXVyNTU1NTcwOTk@._V1_SX300.jpg",
-        Title:"Star Wars: Episode IV - A New Hope",
-        Director:"George Lucas",
+        Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BOTIyMDY2NGQtOGJjNi00OTk4LWFhMDgtYmE3M2NiYzM0YTVmXkEyXkFqcGdeQXVyNTU1NTcwOTk@._V1_SX300.jpg",
+        Title: "Star Wars: Episode IV - A New Hope",
+        Director: "George Lucas",
         Actors: "Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing",
         Released: "25 May 1997",
         Genre: "Action, Adventure, Fantasy"
     };
 
-    it("should output movie result to expected HTML format", function () {
+    var $compile;
+    var $scope;
 
+    beforeEach(module('movie-app'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $scope = _$rootScope_.$new();
+    }));
+
+    it("should output movie result to expected HTML format", function () {
+        var html;
+        html = $compile('<movie-result></movie-result>')($scope).html();
+        expect(html).toBe('<div>Star Wars: Episode IV - A New Hope</div>');
     });
 
 });
