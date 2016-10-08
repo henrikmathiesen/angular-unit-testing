81799b3baf33cb2392b929c184a82547f607a248
Henrik <henrik_mathiesen@hotmail.com>
2016-10-08 21:22:07 +0200

    -template

--- app/js/directives/movie-result.directive.js
+++ app/js/directives/movie-result.directive.js
@@ -9,6 +9,19 @@ angular
             scope: {
                 result: '='
             },
-            template: '<div>{{ result.Title }}</div>'
+            template: [
+                '<div class="row">',
+                    '<div class="col-sm-4">',
+                        '<img ng-src="result.Poster" alt="{{ result.Title }}" />',
+                    '</div>',
+                    '<div class="col-sm-8">',
+                        '<h3>{{ result.Title }}</h3>',
+                        '<p><strong>Director: </strong>{{ result.Director }}</p>',
+                        '<p><strong>Actors: </strong>{{ result.Actors }}</p>',
+                        '<p><strong>Released: </strong>{{ result.Released }}</p>',
+                        '<p><strong>Genre: </strong>{{ result.Genre }}</p>',
+                    '</div>',
+                '</div>'
+            ].join('')
         }
     });
