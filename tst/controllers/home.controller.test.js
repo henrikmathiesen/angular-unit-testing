31328e10c09819e3f0a382473b86fe73f0dda0b7
Henrik <henrik_mathiesen@hotmail.com>
2016-10-08 19:58:51 +0200

    -updated js job to include directives -cleaned up code -begun working directive test for movie data

--- tst/controllers/home.controller.test.js
+++ tst/controllers/home.controller.test.js
@@ -2,29 +2,6 @@
 
 describe("home controller test", function () {
 
-    // var response = {
-    //     data: [
-    //         {
-    //             Title: "Star Wars: Episode IV - A New Hope",
-    //             Year: "1977",
-    //             imdbID: "tt0076759",
-    //             Type: "movie"
-    //         },
-    //         {
-    //             Title: "Star Wars: Episode V - The Empire Strikes Back",
-    //             Year: "1980",
-    //             imdbID: "tt0080684",
-    //             Type: "movie"
-    //         },
-    //         {
-    //             Title: "Star Wars: Episode VI - Return of the Jedi",
-    //             Year: "1983",
-    //             imdbID: "tt0086190",
-    //             Type: "movie"
-    //         }
-    //     ]
-    // };
-
     var response = [
         {
             data: {
---
+++ tst/directives/movie-result.directive.test.js
@@ -0,0 +1,9 @@
+/// <reference path="../../typings/index.d.ts" />
+
+describe("movie-result directive test", function () {
+
+    it("should output movie result to expected HTML format", function () {
+        
+    });
+
+});
