38229659fd9eadbcdc8ba826df798fffa92bcfca
Henrik <henrik_mathiesen@hotmail.com>
2016-10-09 09:34:22 +0200

    -hide image if no src

--- app/js/directives/movie-result.directive.js
+++ app/js/directives/movie-result.directive.js
@@ -12,7 +12,7 @@ angular
             template: [
                 '<div class="row">',
                     '<div class="col-sm-4">',
-                        '<img ng-src="{{ result.Poster }}" alt="{{ result.Title }}" />',
+                        '<img ng-show="result.Poster && result.Poster.length > 3" ng-src="{{ result.Poster }}" alt="{{ result.Title }}" />',
                     '</div>',
                     '<div class="col-sm-8">',
                         '<h3>{{ result.Title }}</h3>',
