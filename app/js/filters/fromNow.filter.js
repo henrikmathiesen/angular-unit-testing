7e9e23fd6e218f50eea356bc86e3a16301a0f0c8
Henrik <henrik_mathiesen@hotmail.com>
2016-10-16 07:57:25 +0200

    -time zone offset for ISO

--- app/js/filters/fromNow.filter.js
+++ app/js/filters/fromNow.filter.js
@@ -5,12 +5,24 @@ angular
     .filter('fromNowFilter', function () {
         return function (value) {
             if(!value) {
-                console.error("ERROR: pass date object or date as string");
-                return "ERROR: value must be date object";
+                return "";
             }
-            
-            return value;
+
+            // With ISO strings, we need to handle time zone offsets
+            if(value.indexOf('T') > -1) {
+                var timeZoneOffSet = new Date().getTimezoneOffset();
+                
+                if(value.indexOf('Z') > -1) {
+                    value = value.split('Z')[0];
+                }
+
+                value = value + timeZoneOffSet;
+            }
+
+            var year = new Date(value).getFullYear();
+            var currentYear = new Date().getFullYear();
+            var yearDiff = currentYear - year;
+
+            return yearDiff == 1 ? "1 year ago" : yearDiff + " years ago"; 
         }
     });
-
-    // Include moment.js
--- tst/filters/from-now.filter.test.js
+++ tst/filters/from-now.filter.test.js
@@ -10,16 +10,33 @@ describe("from-now filter test", function () {
         fromNow = _$filter_('fromNowFilter');
     }));
 
-    it("should return an error for undefined", function () {
-        expect(fromNow()).toBe("ERROR: value must be date object");
+    it("should return an empty string if no value", function () {
+        expect(fromNow()).toBe("");
     });
 
-    it("should return an error for invalid date", function(){
-        expect(fromNow('foo')).toBe("ERROR: value must be date object");
+    it("should return value of years ago for date - 1", function(){
+        var released = "25 May 1986";
+        expect(fromNow(released)).toBe("30 years ago");
     });
 
-    // it("should return value of years ago for date object", function(){
+    it("should return value of years ago for date - 2", function(){
+        var released = "31 Dec 2006";
+        expect(fromNow(released)).toBe("10 years ago");
+    });
 
-    // });
+    it("should return value of 1 year ago for date", function(){
+        var released = "25 May 2015";
+        expect(fromNow(released)).toBe("1 year ago");
+    });
+
+    it("should handle dates as ISO strings - 1", function(){
+        var released = "2006-12-31T23:00:00";
+        expect(fromNow(released)).toBe("10 years ago");
+    });
+
+    it("should handle dates as ISO strings - 2", function(){
+        var released = "2006-12-31T23:00:00.545Z";
+        expect(fromNow(released)).toBe("10 years ago");
+    });
 
 });
