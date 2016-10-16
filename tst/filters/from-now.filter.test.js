4dba6ed93f63d9e6995f5e9cf95e7b545f8fb023
Henrik <henrik_mathiesen@hotmail.com>
2016-10-16 11:21:33 +0200

    -testing date filter with fixed compare values

--- tst/filters/from-now.filter.test.js
+++ tst/filters/from-now.filter.test.js
@@ -16,25 +16,27 @@ describe("from-now filter test", function () {
 
     it("should return value of years ago for date", function () {
         var released = "25 May 1986";
-        expect(fromNow(released)).toBe("30 years ago");
+        var comparedTo = "25 May 2016";
+        expect(fromNow(released, comparedTo)).toBe("30 years ago");
     });
 
     it("should return value of years ago for date close to a new year", function () {
         var released = "31 Dec 2006";
-        expect(fromNow(released)).toBe("10 years ago");
+        var comparedTo = "1 Jan 2016";
+        expect(fromNow(released, comparedTo)).toBe("10 years ago");
     });
 
-    it("should return value of 1 year ago for date of one year before (it should also handle date objects as value)", function () {
-        var now = new Date();
-        var oneYearAgo = now.getTime() - (60 * 60 * 24 * 365 * 1000); // when substracting time (like 1 year, do it in ms, else constructor undefined error)
-        var released = new Date(oneYearAgo);
-
-        expect(fromNow(released)).toBe("1 year ago");
+    it("should return value of 1 year ago singular form", function () {
+        var released = "05 May 2015";
+        var comparedTo = "1 Jan 2016";
+        expect(fromNow(released, comparedTo)).toBe("1 year ago");
     });
 
     it("should handle possible time zone offsets (ISO string)", function () {
         var released = "2006-12-31T23:00:00";
-        expect(fromNow(released)).toBe("10 years ago");
+        var comparedTo = "1 Jan 2016";
+
+        expect(fromNow(released, comparedTo)).toBe("10 years ago");
 
         // Without special logic in the filter, 'released' would be converted to swedish time,
         // which is 1-2 hours (depending on winter/summer time) ahead of UTC time
@@ -42,10 +44,4 @@ describe("from-now filter test", function () {
 
         // But the logic in the filter removes the timeZone offset, "stamping" released to be a local date
     });
-
-    it("should handle dates as ISO strings with Z", function () {
-        var released = "2006-12-31T23:00:00.545Z";
-        expect(fromNow(released)).toBe("10 years ago");
-    });
-
 });
