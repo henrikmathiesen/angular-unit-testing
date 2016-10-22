035918690f6e97fc8fca477112b9052ef8ac84e6
Henrik <henrik_mathiesen@hotmail.com>
2016-10-22 13:22:11 +0200

    -comments

--- app/js/movie-app.module.js
+++ app/js/movie-app.module.js
@@ -22,6 +22,7 @@ angular.module('movie-app', ['ngRoute', 'ngResource', 'ngMockE2E', 'ui.bootstrap
         // We mock ajax calls here
 
         var data = ['tt0076759', 'tt0080684', 'tt0086190'];
+        var header = {};
 
         // This is essential, else normal ajax calls breaks
         $httpBackend.whenGET(/^\w+.*/).passThrough();
@@ -32,13 +33,13 @@ angular.module('movie-app', ['ngRoute', 'ngResource', 'ngMockE2E', 'ui.bootstrap
     /*
         About ngMockE2E module
         - Back end less development
-        - Includes only one service: $httpBackend, which in this context is the in browser variant of the unit test variant 
+        - Includes only one service: $httpBackend, which in this context is the in browser variant of the unit test variant in ngMock
         - Include in module dependencies
         - Reference the script file, 'node_modules/angular-mocks/angular-mocks.js', else error (also comment it out from karma.conf.js so its not included twice in test (lib.js + file array))
         - Handle unexpected GET request
         - Can return mock for all http verbs
         - Can pass through requests to become real request
         - Dont need to flush()
-        - We can NOT use ngMockE2E:s $httpBackend in unit tests 
+        - We can NOT use ngMockE2E:s $httpBackend in unit tests, we have to use ngMock:s $httpBackend, they are in different modules but in the same .js file 
 
      */
