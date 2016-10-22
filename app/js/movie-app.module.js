c220debd203cf05b642e2d529c8cced6ac018de0
Henrik <henrik_mathiesen@hotmail.com>
2016-10-22 13:18:53 +0200

    -comments

--- app/js/movie-app.module.js
+++ app/js/movie-app.module.js
@@ -32,16 +32,13 @@ angular.module('movie-app', ['ngRoute', 'ngResource', 'ngMockE2E', 'ui.bootstrap
     /*
         About ngMockE2E module
         - Back end less development
-        - Includes only one service: $httpBackend
+        - Includes only one service: $httpBackend, which in this context is the in browser variant of the unit test variant 
         - Include in module dependencies
-        - Reference the script file, 'node_modules/angular-mocks/angular-mocks.js', else error
+        - Reference the script file, 'node_modules/angular-mocks/angular-mocks.js', else error (also comment it out from karma.conf.js so its not included twice in test (lib.js + file array))
         - Handle unexpected GET request
         - Can return mock for all http verbs
         - Can pass through requests to become real request
         - Dont need to flush()
         - We can NOT use ngMockE2E:s $httpBackend in unit tests 
-
-        This module gives access to $httpBackend, which in this context is the in browser variant of the unit test variant 
-        _$httpBackend_ which is injectable (when node_modules/angular-mocks/angular-mocks.js is in karma.conf.js of course) 
 
      */
