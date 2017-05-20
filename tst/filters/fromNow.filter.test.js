256bdc85c2a6c2e08de6c48d3c514efe369ece97
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-05-20 12:05:47 +0200

    -updated test

--- tst/filters/fromNow.filter.test.js
+++ tst/filters/fromNow.filter.test.js
@@ -1,6 +1,6 @@
 /// <reference path="../../typings/index.d.ts" />
 
-fdescribe("from-now filter test", function () {
+describe("from-now filter test", function () {
 
     var fromNow;
 
@@ -14,15 +14,41 @@ fdescribe("from-now filter test", function () {
         expect(fromNow()).toBe("");
     });
 
-    it("expects to be given an UTC time", function () { 
+    it("expects to be given an UTC time", function () {
         var released = "2015-05-05T00:00:00+00:00";
         var comparedTo = "2016-01-01T00:00:00+00:00";
         expect(fromNow(released, comparedTo)).toBe("1 year ago");
     });
 
-    it("can fail if not given an UTC time", function () { 
+    it("can fail if not given an UTC time, since date will be converted to UTC time", function () {
         var released = "2015-05-05T00:00:00+02:00";
-        var comparedTo = "2016-01-01T00:00:00+02:00";
+        var comparedTo = "2016-01-01T00:00:00+01:00";
+        expect(fromNow(released, comparedTo)).not.toBe("1 year ago");
+    });
+});
+
+describe("from-now filter v2 test, comparison", function () {
+    var fromNow;
+
+    beforeEach(module('movie-app'));
+
+    beforeEach(inject(function (_$filter_) {
+        fromNow = _$filter_('fromNowFilterV2');
+    }));
+
+    it("should return an empty string if no value", function () {
+        expect(fromNow()).toBe("");
+    });
+
+    it("expects to be given a local time", function () {
+        var released = "2015-05-05T00:00:00+02:00";
+        var comparedTo = "2016-01-01T00:00:00+01:00";
+        expect(fromNow(released, comparedTo)).toBe("1 year ago");
+    });
+
+    it("can fail if not given an local time, since date will be converted to local time", function () {
+        var released = "2015-12-31T23:00:00+00:00";
+        var comparedTo = "2016-01-01T00:00:00+00:00";
         expect(fromNow(released, comparedTo)).not.toBe("1 year ago");
     });
 });
